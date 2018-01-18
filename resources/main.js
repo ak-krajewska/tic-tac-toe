a8a9528f8e6048adb7c3cd687d1d6d06f94656f2
AK Krajewska <akrajewska@gmail.com>
2018-01-17 16:58:59 -0800

    player clicks squares to add tokens

--- resources/main.js
+++ resources/main.js
@@ -1,3 +1,5 @@
+//this game was created with help from the JavaScript Tic Tac Toe Project Tutorial
+
 var scoreBoard = []; //array that tracks what place has what mark
 var human = 'O'; //this is the default we'll add an interaction to let the player choose X or 0
 var computer = 'X';
@@ -16,26 +18,43 @@ startGame(); //if you don't do this nothing works, because the event handlers ar
 
 
 //create the function that starts the game which does the following
+function startGame(){
     //set the endgame pop-up to invistible at the start
-    //populate the scoreBoard array using Array.from and Array.keys
+    document.querySelector(".endgame").style.display = "none";
+    //populate the scoreBoard array 
+    scoreBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     //iterate through all the visible cells and do the following things
+    for (var i = 0; i < cells.length; i++){
         //make them blank
+        cells[i].innerHTML = '';
         //clear them of color (which we use later to mark a winner)
+        cells[i].style.backgroundColor = '';
         //add an event listener to each cell that calls another function (turnClick) that contorls what happens when you click
-
+        cells[i].addEventListener('click', turnClick, false); //what does false do???
+    }
+}
+   
 //create the function that controls what happens when you click on a square (takes a parameter "square" so you know what square you're manipulating)
+function turnClick(square){
+    console.log("You have clicked " + square.target.id);
     //check that no one has played in the suare yet if true then do:
         //the human takes their turn using the turn() function, which takes two paramaters, the id of the square, and the player (as in human or computer)
+        //the reason we don't go directly to "turn" is it can be called by the human or the computer
+        turn(square.target.id, human);
         //the computer takes their turn
             //the computer checks if it's a tie, and if it's not a tie
             //the computer takes its turn using the turn function, which takes two paramaters, the id of the suare and the player
-
+}
 
 //create the turn function, which take two paramaters: the id of the square in play, and the player
+function turn(squareId, player){
     //update the place in the scorekeeping array with the token of the current player (for example human is O, clics the middle top square now the array is 0 O 2 3 4 5 6 7 8
+    scoreBoard[squareId] = player;
+    cells[squareId].innerHTML = player;
     //update the visual display so the player's token appears in the square (hint: use innerText)
     //use a checkWin function (which takes 2 parameters, scoreboard, and player); assign the output of the function to a local variable (gameWon) using the "let" keywor (instead of "var") (this will return either null which is falsy or some object, I think, which is truthy
     //if gameWon is truthy, pass the gameWon object into the gameOver function
+}
 
 //create the checkwin() function which takes two paramaters: the state of the scoreboard, and the current player
     //create a local variable plays, which is an array, which contains only those plays on the scoreboard that were placed by the current player --> hint, use Array.reduce on the board, also this is where the original uses ES6 style but you do you
